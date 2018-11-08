905662aad6d316c395ff3c83f1b2c8bafbb6b43c
chenguokai <1415430544@qq.com>
2018-11-08 21:03:45 +0800

    Initial Commit

---
+++ 386test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  386test
+//
+//  Created by Xim on 2018/11/8.
+//  Copyright © 2018 Xim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
